24ae2595583c502dcc93546e8160e31bd18f8c0f
Maxime Pigeon <me@maximepigeon.com>
2023-07-10 18:44:41 -0400

    add details

--- example-block.js
+++ example-block.js
@@ -5,29 +5,27 @@ export default class ExampleBlock extends HTMLElement {
 	}
 
 	connectedCallback() {
-		// const name = this.getAttribute("name") || "Exemple";
+		const name = this.getAttribute("name") || "Exemple";
 
 		this.style.display = "block";
 		this.style.margin = "1rem 0";
-		// this.style.padding = "1rem";
-		// this.style.backgroundColor = "whitesmoke";
 
 		const style = this.querySelector("style");
 		if (style) style.remove();
 
-		// const details = document.createElement("details");
-		// details.innerHTML = this.innerHTML;
+		const details = document.createElement("details");
+		details.innerHTML = this.innerHTML;
 
-		// const summary = document.createElement("summary");
-		// summary.textContent = name;
-		// summary.style.cursor = "pointer";
-		// details.prepend(summary);
+		const summary = document.createElement("summary");
+		summary.textContent = name;
+		summary.style.cursor = "pointer";
+		details.prepend(summary);
 
-		// const adjacentToSummary = details.querySelector("summary + *");
-		// adjacentToSummary.style.marginTop = "1rem";
+		const adjacentToSummary = details.querySelector("summary + *");
+		adjacentToSummary.style.marginTop = "1rem";
 
-		this.shadowRoot.innerHTML = this.innerHTML;
-		if (style) this.shadowRoot.prepend(style);
+		if (style) this.shadowRoot.append(style);
+		this.shadowRoot.append(details);
 	}
 }
 
